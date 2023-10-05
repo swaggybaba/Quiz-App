5e0552e7621ee5136f7aad10df3bc136391d68df
Prafful Chauhan <praffulchauhan49@gmail.com>
2023-10-05 21:33:30 +0530

    Added Exception handling with HTTP codes

--- src/main/java/com/prafful/demo/controller/QuestionController.java
+++ src/main/java/com/prafful/demo/controller/QuestionController.java
@@ -3,6 +3,7 @@ package com.prafful.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,17 +23,17 @@ public class QuestionController {
 	QuestionService questionService;
 	
 	@GetMapping("allquestions")
-	public List<Question> getAllQuestions() {
+	public ResponseEntity<List<Question>> getAllQuestions() {
 		return questionService.getAllQuestions();
 	}
 	
 	@GetMapping("category/{category}")
-	public List<Question> getQuestionsByCategory(@PathVariable String category){
+	public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category){
 		return questionService.getQuestionsByCategory(category);
 	}
 	
 	@PostMapping("add")
-	public String addQuestion(@RequestBody Question question) {
+	public ResponseEntity<String> addQuestion(@RequestBody Question question) {
 		return questionService.addQuestion(question);
 	}
 }
--- src/main/java/com/prafful/demo/service/QuestionService.java
+++ src/main/java/com/prafful/demo/service/QuestionService.java
@@ -1,8 +1,11 @@
 package com.prafful.demo.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.prafful.demo.Question;
@@ -13,16 +16,31 @@ public class QuestionService {
 	
 	@Autowired
 	QuestionDao questionDao;
-	public List<Question> getAllQuestions() {
+	public ResponseEntity<List<Question>> getAllQuestions() {
+		try {
+			return new ResponseEntity<>(questionDao.findAll(),HttpStatus.OK);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+	}
+	public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+		try {
+			return new ResponseEntity<>(questionDao.findByCategory(category) ,HttpStatus.OK);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+	}
+	public ResponseEntity<String> addQuestion(Question question) {
 		
-		return questionDao.findAll();
-	}
-	public List<Question> getQuestionsByCategory(String category) {
-		return questionDao.findByCategory(category);
-	}
-	public String addQuestion(Question question) {
-		questionDao.save(question);
-		return "success";
+		try {
+			questionDao.save(question);
+			return new ResponseEntity<>("success",HttpStatus.CREATED);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>("",HttpStatus.BAD_REQUEST);
 	}
 
 }
